51996a062d09f64fb6d837bd01c2ea033d2b9c00
Denis Valladares <denisvalladares402@gmail.com>
2023-07-19 15:00:11 -0600

    add component quantity

---
+++ src/app/core/components/quantity/quantity.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dv-quantity',
+  templateUrl: './quantity.component.html',
+  styleUrls: ['./quantity.component.scss']
+})
+export class QuantityComponent {
+
+  public quantity: number = 1;
+}
