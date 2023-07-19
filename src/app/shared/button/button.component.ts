83a844203778185a4b5f6253e7a66e72e0e7f331
Denis Valladares <denisvalladares402@gmail.com>
2023-07-18 21:59:20 -0600

    add custom type button and props

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+export type TypeButton = "primary" | "secondary" | "link"
 @Component({
   selector: 'dv-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
-
+  @Input() type: TypeButton = "primary";
+  @Output() click = new EventEmitter<void>();
 }
