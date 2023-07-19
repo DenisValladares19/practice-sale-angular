af9f983143e6fac40110c2b224da998dfea87d7c
Denis Valladares <denisvalladares402@gmail.com>
2023-07-18 22:29:18 -0600

    refactor button

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -9,4 +9,16 @@ export type TypeButton = "primary" | "secondary" | "link"
 export class ButtonComponent {
   @Input() type: TypeButton = "primary";
   @Output() click = new EventEmitter<void>();
+
+  public getClassNameByType(t: TypeButton): string {
+    if (t === 'link') {
+      return "w-full text-sky-600"
+    }
+
+    if (t === 'secondary') {
+      return "w-full text-slate-600 bg-white p-2 rounded-md border-slate-600 border focus:bg-slate-500 hover:bg-slate-600 hover:text-white focus:text-white";
+    }
+
+    return "w-full text-white bg-slate-600 p-2 rounded-md focus:bg-slate-400 hover:bg-slate-500";
+  }
 }
