b6695523fddcedce2f767a303efbb1b9879fa90f
Denis Valladares <denisvalladares402@gmail.com>
2023-07-18 22:34:31 -0600

    add align button text

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -9,16 +9,21 @@ export type TypeButton = "primary" | "secondary" | "link"
 export class ButtonComponent {
   @Input() type: TypeButton = "primary";
   @Output() click = new EventEmitter<void>();
+  @Input() align: "start" | 'center' = 'center';
 
-  public getClassNameByType(t: TypeButton): string {
-    if (t === 'link') {
-      return "w-full text-sky-600"
+
+  public getClassNameByType(): string {
+    let className = "w-full text-white bg-slate-600 p-2 rounded-md focus:bg-slate-400 hover:bg-slate-500";
+
+    if (this.type === 'link') {
+      className = "w-full text-sky-600";
+
     }
 
-    if (t === 'secondary') {
-      return "w-full text-slate-600 bg-white p-2 rounded-md border-slate-600 border focus:bg-slate-500 hover:bg-slate-600 hover:text-white focus:text-white";
+    if (this.type === 'secondary') {
+      className = "w-full text-slate-600 bg-white p-2 rounded-md border-slate-600 border focus:bg-slate-500 hover:bg-slate-600 hover:text-white focus:text-white";
     }
 
-    return "w-full text-white bg-slate-600 p-2 rounded-md focus:bg-slate-400 hover:bg-slate-500";
+    return `${className} text-${this.align}`;
   }
 }
