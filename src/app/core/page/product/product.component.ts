41e472883dd5de2012eb39ad6b2350947da2e999
Denis Valladares <denisvalladares402@gmail.com>
2023-07-27 11:37:31 -0600

    show loading in product page

--- src/app/core/page/product/product.component.ts
+++ src/app/core/page/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductDTO } from 'src/app/interfaces/Product.model';
 import { methodEnum } from 'src/app/interfaces/Request.model';
 import { ApiService } from 'src/app/services/api.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'dv-product',
@@ -11,11 +12,18 @@ import { ApiService } from 'src/app/services/api.service';
 export class ProductComponent implements OnInit {
   public listProduct: ProductDTO[] = [];
 
-  constructor(private api: ApiService<ProductDTO[]>) {}
+  constructor(
+    private api: ApiService<ProductDTO[]>,
+    private loading: LoadingService
+    ) {}
 
   ngOnInit() {
+    this.loading.show();
     this.api.fetch({ url: "/product", method: methodEnum.GET }).subscribe((res) => {
       this.listProduct = res.content || [];
+      this.loading.hidden();
+    }, (err) => {
+      this.loading.hidden();
     })
   }
 
