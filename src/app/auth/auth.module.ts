ba1c0076336fd3583075f04f92548c471414816b
Denis Valladares <denisvalladares402@gmail.com>
2023-07-27 12:14:56 -0600

    add AuthRouting module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SignupComponent } from './signup/signup.component';
 
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 @NgModule({
-  declarations: [
-    SignupComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
