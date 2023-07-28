fc70cd32804573b1d074d2e69cc248ea4e79b028
Denis Valladares <denisvalladares402@gmail.com>
2023-07-27 20:23:30 -0600

    fix styles button

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -15,15 +15,15 @@ export class ButtonComponent {
 
 
   public getClassNameByType(): string {
-    let className = "w-full text-white bg-slate-600 py-2 rounded-md focus:bg-slate-400 hover:bg-slate-500 flex justify-between px-5 disabled:opacity-50";
+    let className = "w-full text-white bg-slate-600 py-2 rounded-md focus:bg-slate-400 hover:bg-slate-500 flex justify-center px-5 disabled:opacity-50";
 
     if (this.type === 'link') {
-      className = "w-full text-sky-600 flex justify-between items-center disabled:opacity-50";
+      className = "w-full text-sky-600 flex justify-center items-center disabled:opacity-50";
 
     }
 
     if (this.type === 'secondary') {
-      className = "w-full text-slate-600 bg-white py-2 rounded-md border-slate-600 border focus:bg-slate-500 hover:bg-slate-600 hover:text-white focus:text-white flex justify-between px-5 disabled:opacity-50";
+      className = "w-full text-slate-600 bg-white py-2 rounded-md border-slate-600 border focus:bg-slate-500 hover:bg-slate-600 hover:text-white focus:text-white flex justify-center px-5 disabled:opacity-50";
     }
 
     return `${className} text-${this.align}`;
